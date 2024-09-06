f40d4b6acb99ce37761b414b45093e0fa284465e
hyun <apfhd0257@naver.com>
2024-09-06 15:07:25 +0900

    퀵 정렬 - 퀵 정렬을 이용한 문제 풀이(11004)

--- src/cordingTest/doitCordingTest/sort/Q19_quickSort.java
+++ src/cordingTest/doitCordingTest/sort/Q19_quickSort.java
@@ -21,9 +21,66 @@ public class Q19_quickSort {
         for (int i=0; i<n; i++) {
             a[i] = Integer.parseInt(st.nextToken());
         }
-        
-        Arrays.sort(a);
-        
+
+        quickSort(a, 0, n-1, k-1);
+
         System.out.println(a[k-1]);
+
+    }
+
+    static void quickSort(int[] a, int start, int end, int k) {
+        if (start < end) {
+            int pivot = partition(a, start, end);
+            if (pivot == k) {
+                return;
+            }
+            else if (k < pivot) {
+                quickSort(a, start, pivot-1, k);
+            } else {
+                quickSort(a, pivot+1, end, k);
+            }
+        }
+    }
+
+    static int partition(int[] a, int start, int end) {
+        // 이 조건은 무엇?
+        if (start + 1 == end) {
+            if (a[start] > a[end]) {
+                swap(a, start, end);
+            }
+            return end;
+        }
+
+        int m = (start + end) / 2;
+        int pivot = a[m];
+        int pivot_index = start;
+
+        swap(a, start, m);
+
+        start++;
+
+        while (start < end) {
+            while (pivot < a[end]) {
+                end--;
+            }
+
+            while (pivot > a[start] && start <= end) {
+                start++;
+            }
+
+            if (start <= end) {
+                swap(a, start, end);
+                start++;
+                end--;
+            }
+        }
+        swap(a, pivot_index, end);
+        return end;
+    }
+
+    static void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
     }
 }
