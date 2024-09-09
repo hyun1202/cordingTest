5cec8e08e65615fff6a6e18a8c61307393d058b8
hyun <apfhd0257@naver.com>
2024-09-09 19:59:21 +0900

    기수 정렬(10989) - 시간 초과 및 메모리 초과 해결  - 1. 시간 초과 - Scanner, System.out -> BufferedReader 및 BufferedWriter로 수정  - 2. 메모리 초과 - Queue -> 기본 배열로 수정 (합 배열 이용)

--- src/cordingTest/doitCordingTest/sort/Q22.java
+++ src/cordingTest/doitCordingTest/sort/Q22.java
@@ -1,38 +1,57 @@
 package cordingTest.doitCordingTest.sort;
 
+import java.io.*;
 import java.util.*;
 
+/**
+ * 백준 10989
+ * 1. 시간 초과 - Scanner, System.out -> BufferedReader 및 BufferedWriter로 수정
+ * 2. 메모리 초과 - Queue -> 기본 배열로 수정 (합 배열 이용)
+ */
 public class Q22 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = sc.nextInt();
+        int n = Integer.parseInt(br.readLine());
 
         int[] a = new int[n];
 
         for (int i=0; i<n; i++) {
-            a[i] = sc.nextInt();
+            a[i] = Integer.parseInt(br.readLine());
         }
 
         int maxSize = 5;
 
-        Queue<Integer>[] bucket = new LinkedList[10];
-
-        for (int i=0; i<10; i++) {
-            bucket[i] = new LinkedList<>();
-        }
+//        // 메모리 초과로 Queue 사용 X
+//        Queue<Integer>[] bucket = new LinkedList[10];
 
         int count = 0;
         int radix = 1;
         while (count < maxSize) {
+            int[] output = new int[n];
+            int[] bucket = new int[10];
+
             for (int i=0; i<n; i++) {
-                bucket[a[i] / radix % 10].add(a[i]);
+                bucket[a[i] / radix % 10]++;
             }
 
-            for (int i=0, j=0; i<bucket.length; i++) {
-                while (!bucket[i].isEmpty()) {
-                    a[j++] = bucket[i].poll();
-                }
+            for(int i=1; i<10; i++) {
+                // 합배열 이용해 첫 index를 계산한다.
+                // 예를들어 bucket[3] = 4, bucket[4] = 7라면
+                // 자릿수 3에 해당하는 값들은 인덱스 3부터 시작하고, 자릿수 4에 해당하는 값들은 인덱스 6부터 시작한다는 의미 (배열의 인덱스는 0부터이므로 -1)
+                bucket[i] += bucket[i-1];
+            }
+
+            // 현재 자리수를 기준으로 정렬
+            for (int i=n-1; i>=0; i--) {
+                // 위에서 구한 index로 정렬 시작
+                // 한번 구하면 index--를 해준다.
+                output[bucket[a[i] / radix % 10]-- -1] = a[i];
+            }
+
+            for (int i=0; i<n; i++) {
+                a[i] = output[i];
             }
 
             radix *= 10;
@@ -40,7 +59,11 @@ public class Q22 {
         }
 
         for (int i=0; i<n; i++) {
-            System.out.println(a[i]);
+            bw.write(a[i]+"\n");
         }
+
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
